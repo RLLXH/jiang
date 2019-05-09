15c70f5854cd253d06fa65a71c04c79b218857b7
jiang <948742327@qq.com>
2019-05-09 11:39:12 +0800

    江提交

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { Message } from 'element-ui';
-axios.defaults.baseURL = 'http://localhost:8888'
+axios.defaults.baseURL = 'http://127.0.0.1:8888'
 
 axios.defaults.withCredentials = true;
 //添加请求拦截器
